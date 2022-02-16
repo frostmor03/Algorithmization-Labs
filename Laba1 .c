97eadf9fb7c51fc3fc746416ac66a2fe705d0c0c
user <you@example.com>
2022-02-16 16:15:38 +0500

    please help

---
+++ Laba1 .c
@@ -0,0 +1,15 @@
+# include  <stdio.h>
+# include  <math.h>
+int main()
+{
+  float a, b, c;
+  printf ("Enter a -> ");
+  scanf ("%f", &a);
+  printf ("Enter b -> ");
+  scanf ("%f", &b);
+  printf ("Enter c -> ");
+  scanf ("%f", &c);
+  if (!a && !b && !c)
+          return 0;
+  float x = 1.0f;
+  printf("f(x) = ");
